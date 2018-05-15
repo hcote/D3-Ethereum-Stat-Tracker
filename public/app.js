d37d69b38fb5d4805c259370926424f5ef025d1d
hcote <hcote12@gmail.com>
2018-05-15 16:41:19 -0700

    style fixes

--- public/app.js
+++ public/app.js
@@ -1,70 +1,66 @@
 $( document ).ready(function() {
     console.log( "jQuery is linked" );
 
+// start
+
+
 $('.expand_block').on('click', function() {
   console.log(this.value);
 
     $.ajax({
        method: 'GET',
-       url: `https://api.blockcypher.com/v1/btc/main/blocks/${this.name}?txstart=1&limit=10`,
-       success: handleSuccess,
+       url: `https://api.blockcypher.com/v1/btc/main/blocks/${this.name}?txstart=1&limit=5`,
+       success: blockData,
        error: handleError
      });
    })
 
-     function handleSuccess(json) {
+// end
+// start
+
+     function blockData(json) {
        console.log(json);
        console.log(json.txids);
        var id = `${json.height}`;
        $(`#${id}`).addClass('focused_block');
        $(`#${id}`).removeClass('unfocused_block');
        $(`.${id}`).addClass('hide');
-       $('.collapse_block').removeClass('hide');
+       $(`.collapse_block${id}`).removeClass('hide');
        $(`#${id}`).append(`
          <section class="tx_data">
-           <h2>Block Head</h2>
-           <p>Timestamp: <span class="data"> ${json.time}</span></p>
-           <p>Height: <span class="data"> ${json.height}</span></p>
-           <p>Nonce: <span class="data"> ${json.nonce}</span></p>
+           <h3>Block Head</h3>
+           <p class="inline">Timestamp: <span class="data"> ${json.time}</span></p>
+           <p class="inline">Height: <span class="data"> ${json.height}</span></p>
+           <p class="inline">Nonce: <span class="data"> ${json.nonce}</span></p>
+           <p class="inline_block">Version: <span class="data"> ${json.ver}</span></p>
+           <p class="inline">Bits: <span class="data"> ${json.bits}</span></p>
            <p>Merkle Root: <span class="data"> ${json.mrkl_root}</span></p>
-           <p>Previous Block Hash: <span class="data"> ${json.prev_block}</span></p>
            <p>This Block Hash: <span class="data"> ${json.hash}</span></p>
+           <p>Previous Block Hash: <span class="data"> ${json.prev_block}</span></p>
            <hr>
-           <p class="tx_ids_head">TX IDs:</p>
-              <details><summary><span>${json.txids[0]}</span></summary>
-              <p>Sending Address: </p>
-              <p>Receiving Address: </p>
-              <p>Amount BTC: </p>
-              </details>
-              <details><summary><span>${json.txids[1]}</span></summary>
-              <p>Sending Address: </p>
-              <p>Receiving Address: </p>
-              <p>Amount BTC: </p>
-              </details>
-              <details><summary><span>${json.txids[2]}</span></summary>
-              <p>Sending Address: </p>
-              <p>Receiving Address: </p>
-              <p>Amount BTC: </p>
-              </details>
-              <details><summary><span>${json.txids[3]}</span></summary>
-              <p>Sending Address: </p>
-              <p>Receiving Address: </p>
-              <p>Amount BTC: </p>
-              </details>
-              <details><summary><span>${json.txids[4]}</span></summary>
-              <p>Sending Address: </p>
-              <p>Receiving Address: </p>
-              <p>Amount BTC: </p>
-              </details>
+           <h3>Block Body</h3>
+           <h4 class="tx_ids_head">Transaction Hashes (Tx IDs):</h4>
+            <input class="tx_input one" type="text" value="${json.txids[0]}"></input>
+            <input class="tx_input two" type="text" value="${json.txids[1]}"></input>
+            <input class="tx_input three" type="text" value="${json.txids[2]}"></input>
+            <input class="tx_input four" type="text" value="${json.txids[3]}"></input>
+            <input class="tx_input five" type="text" value="${json.txids[4]}"></input>
              <br>
             </section>
-             `)
+             `);
+
            };
+
+    // end
+    // start
 
      function handleError(err) {
        alert('Hourly limit of 200 requests reached. Try again at the top of the hour')
        console.log(err);
      }
+
+    // end
+    // start
 
      $('.collapse_block').on('click', function() {
        console.log(this);
@@ -75,4 +71,49 @@ $('.expand_block').on('click', function() {
        $(this).siblings().html('');
      })
 
+     // end
+     // start
+
+     $('.details').on('click', function() {
+       console.log('hello');
+       console.log(this.value);
+       $.ajax({
+          method: 'GET',
+          url: `https://blockchain.info/rawtx/b6287895419021a1e4a1e067510971dae1563105075b4f13935b1c5c747eeca8${this.value}`,
+          success: txData,
+          error: handleError
+        });
+      })
+
+      function txData(json) {
+        console.log(this);
+      }
+
+      // end end
 })
+
+// <details><summary><span>${json.txids[0]}</span></summary>
+// <p>Sending Address: </p>
+// <p>Receiving Address: </p>
+// <p>Amount BTC: </p>
+// </details>
+// <details><summary><span>${json.txids[1]}</span></summary>
+// <p>Sending Address: </p>
+// <p>Receiving Address: </p>
+// <p>Amount BTC: </p>
+// </details>
+// <details><summary><span>${json.txids[2]}</span></summary>
+// <p>Sending Address: </p>
+// <p>Receiving Address: </p>
+// <p>Amount BTC: </p>
+// </details>
+// <details><summary><span>${json.txids[3]}</span></summary>
+// <p>Sending Address: </p>
+// <p>Receiving Address: </p>
+// <p>Amount BTC: </p>
+// </details>
+// <details><summary><span>${json.txids[4]}</span></summary>
+// <p>Sending Address: </p>
+// <p>Receiving Address: </p>
+// <p>Amount BTC: </p>
+// </details>
--- readme.md
+++ readme.md
@@ -9,4 +9,13 @@ My goal is to create an educational and interactive web app to update real-time 
 Many people interested in learning more about cryptocurrencies and blockchian are turned off when it's explained in a way that their first reaction is, "You have to be a computer scientist to understand this stuff". Wider adoption of cryptocurrencies cannot occur until people truly understand what a blockchain is and how it works.
 
 ## Technologies
-For now, simple node.js backend and ejs front end. I am using axios API calls to blockchain.info as well as jQuery AJAX code to run blockcypher's API calls for my data. 
+For now, simple node.js backend and ejs front end. I am using axios API calls to blockchain.info as well as jQuery AJAX code to run blockcypher's API calls for my data.
+
+## Planned Features
+* User is able to click at a tx ID and see the sending and receiving addresses, as well as the BTC transfered
+* Timer showing elapsed time since last hashed block
+* Animation of new block entering the chain if user is on site while a new block is mined
+* Animation of a potential miner attempting to approve the block
+* Interactive - user can edit the tx IDs, which will change the block hash, making the outline red and rendering it incorrect, showing the immutability of a block chain.
+* Info bubbles if you hover over any key words showing definitions (nonce, merkle root, height, etc.)
+* Re-write it in React.js
